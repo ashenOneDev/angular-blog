109b65bdee857bde36fa90eb0f157b03e2aa2834
ioce <iago.geraldo@gft.com>
2023-01-19 09:36:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "image": "https://wiki.tripwireinteractive.com/images/4/47/Placeholder.png",
+    "title": "Lorem Ipsum",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam malesuada turpis eu ipsum commodo, eget interdum dui mattis. Duis feugiat enim lacinia congue tempus. Sed aliquam imperdiet venenatis. Vestibulum eget massa ullamcorper, fringilla mauris vel, vulputate nunc. Donec faucibus nunc ut lorem vulputate fermentum."
+  },
+  {
+    "id": 2,
+    "image": "https://wiki.tripwireinteractive.com/images/4/47/Placeholder.png",
+    "title": "Teste 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "description":"Description 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    "id": 3,
+    "image": "https://wiki.tripwireinteractive.com/images/4/47/Placeholder.png",
+    "title": "Teste 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "description":"Description 2: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    "id": 4,
+    "image": "https://wiki.tripwireinteractive.com/images/4/47/Placeholder.png",
+    "title": "Teste 3 Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "description":"Description 3: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  }
+]
